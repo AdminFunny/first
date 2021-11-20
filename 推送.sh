42629cc621dc39bcf8f198ff61a01570c4d68189
Funny <funny@hcat.work>
2021-11-20 20:37:49 +0800

    自动推送

---
+++ 推送.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出所有错误
+
+set -e
+
+# 发布到自定义域名
+
+echo 'xxxx.xxx.com' > CNAME
+
+git init
+git add -A
+git commit -m '自动推送'
+
+# Github
+git push -f git@github.com:AdminHcat/MiniworldHack.git master:website
